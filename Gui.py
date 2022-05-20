1d2f24de01edf9c74b4d041084ef00a8512584ba
jmjumper <juli.springer@gmx.de>
2022-05-20 02:35:44 +0200

    first commit

---
+++ Gui.py
@@ -0,0 +1,187 @@
+import getpass
+import os
+import pathlib
+from tkinter import *
+import tkinter as tk
+from tkinter import IntVar, StringVar, filedialog
+from tkinter.simpledialog import askstring
+import tkinter.ttk
+from pystray import MenuItem as item
+import pystray
+from PIL import Image, ImageTk
+from downloadOrganizer import File_Handler
+from win32api import GetSystemMetrics
+
+global runner
+
+width = int ( GetSystemMetrics(0) / 3.5 )
+height = int ( GetSystemMetrics(1) / 2 )  
+
+class GUI(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title('Make your Download-Folder great again!')
+        self.geometry(f'{width}x{height}')
+        self.iconbitmap('./img/icon.ico')
+        # self.resizable(False, False)
+        self.download_folder = runner.get_download_directory()
+        self.destination_folder = runner.get_destination_directory()
+        self.categories_dict = runner.get_categories_extensions()
+        self.interval = runner.get_interval()
+        self.protocol("WM_DELETE_WINDOW", self.hide_window)
+        self.grid_columnconfigure(2, weight=1)
+        self.setup_gui()
+    def update_values(self, current_cat):
+        to_add = askstring(f'Add extension', f'Add an extension to category: "{current_cat}"')
+        print(f'add {to_add} to {current_cat}')
+    def index_categories(self, grid):
+        row = 0
+        btn_list = []
+        for k, v in runner.get_categories_extensions().items():
+            tk.Label(grid, text=f'{k}:', font=('Arial', int ( 10 ), 'bold')).grid(column=0, row=row, padx=(20, 0), pady=(0, 1), sticky='w')
+            if v is not None:
+                values = ''
+                for i in v:
+                    if values == '':
+                        values = i
+                    else:
+                        values += f', {i}' 
+                tk.Label(grid, text=f'{values},', font=('Arial', int ( 10 ), 'bold')).grid(column=1, row=row, padx=(20, 0), pady=(0, 1), sticky='w')
+            else:
+                tk.Label(grid, text=f'none', font=('Arial', int ( 10 ), 'italic')).grid(column=1, row=row, padx=(20, 0), pady=(0, 1), sticky='w')
+            row += 1
+    def activate_autostart(self):
+        file_path = os.path.dirname(os.path.realpath(__file__))
+        ext = os.fsdecode(os.path.realpath(__file__))
+        USER_NAME = getpass.getuser()
+        bat_path = r'C:/Users/%s/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup' % USER_NAME
+        print(ext)
+        with open(bat_path + '/' + "DownloadsOrganizer.bat", "w+") as bat_file:
+            bat_file.write(f'start "" "{file_path}{ext}"')
+    def deactivate_autostart(self):
+        USER_NAME = getpass.getuser()
+        bat_path = r'C:/Users/%s/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup' % USER_NAME
+        if os.path.exists(bat_path + '/' + "DownloadsOrganizer.bat"):
+            os.remove(bat_path + '/' + "DownloadsOrganizer.bat")
+    def done(self):
+        runner.set_download_directory(self.download_folder)
+        runner.set_destination_directory(self.destination_folder)
+    def save(self, interval, autostart):
+        self.interval = interval
+        runner.set_interval(interval)
+        if autostart:
+            self.activate_autostart()
+        else:
+            self.deactivate_autostart()
+        self.done()
+        tk.messagebox.showinfo(title='Saved', message='Saved all settings. Click on \'start\' to start the program.')
+    def start(self):
+        runner.start()
+        tk.messagebox.showinfo(title='Starting...', message='Started the program.')
+    def stop(self):
+        runner.stop()
+        tk.messagebox.showinfo(title='Stopping...', message='Successfully stopped the program.')
+    def init_settings(self, grid):
+        interval_lbl = tk.Label(grid, text='Interval: ', font=('Arial', int ( 10 ), 'bold'))
+        interval_lbl.grid(column=0, row=0, padx=(20, 0))
+        
+        placeholder_spinbox = StringVar(self)
+        placeholder_spinbox.set(int(self.interval))
+        spinbox = tk.Spinbox(grid, from_=1, to=1000, width=5, textvariable=placeholder_spinbox)
+        spinbox.grid(column=1, row=0, padx=(5, 0))
+        
+        is_autostart = tk.IntVar()
+        opt_autostart = tk.Checkbutton(grid, text='Autostart', variable=is_autostart)
+        opt_autostart.grid(row=0, column=2, padx=(20,0))
+        
+        save_btn = tk.Button(grid, text='Save', command=lambda : self.save(spinbox.get(), is_autostart.get()))
+        save_btn.grid(column=0, row=1, sticky='w', padx=(20,0), pady=(10,0))
+        
+        start_btn = tk.Button(grid, text='Start', command=self.start)
+        start_btn.grid(column=1, row=1, sticky='w', padx=(40,0), pady=(10,0))
+        
+        stop_btn = tk.Button(grid, text='Stop', command=self.stop)
+        stop_btn.grid(column=2, row=1, sticky='w', padx=(20,0), pady=(10,0))
+    def select_download_folder(self, lbl):
+        self.download_folder = filedialog.askdirectory()
+        lbl['text'] = f'{self.download_folder}'
+    def select_destination_folder(self, lbl):
+        self.destination_folder = filedialog.askdirectory()
+        lbl['text'] = f'{self.destination_folder}'
+    def help(self):
+        tk.messagebox.showinfo(title='Extensions', message='Please navigate to the installation folder and find the \'settings.ini\' file.\nTo add a category, append a new line at the bottom. To add an extension, append them to the according line, separated by a comma. Please find the included examples.')
+    def quit_window(self, icon, item):
+        icon.stop()
+        runner.stop()
+        self.destroy()
+    def show_window(self, icon, item):
+        icon.stop()
+        self.after(0, self.deiconify())
+    def hide_window(self):
+        self.withdraw()
+        image = Image.open('./img/icon.ico')
+        menu = (item('Show', self.show_window), item('Quit', self.quit_window))
+        icon=pystray.Icon('Downloadfolder Organizer', image, 'Organizer', menu)
+        icon.run()
+    def setup_gui(self):
+        # --------- MAIN GRID ---------
+        grid = tk.Frame(self, bd=2)
+        grid.pack(fill='x')
+        
+        # --------- FIRST ROW ---------
+        grid1 = tk.Frame(grid, bd=2)
+        grid1.grid(column=0, row=0, padx=0, pady=(10, 5), sticky='w')
+        
+        grid1.grid_columnconfigure(1, weight=1)
+        
+        foldersetting_lbl = tk.Label(grid1, text='Folder-Settings', font=('Arial', int ( width / 25 ), 'bold'))
+        foldersetting_lbl.grid(column=0, row=0, padx=20, sticky='w')
+
+        downloadfolder_lbl = tk.Label(grid1, text='Select your Download-Folder...', font=('Arial', int ( 10 ), 'bold'))
+        downloadfolder_lbl.grid(column=0, row=1, padx=20, pady=(10,0), sticky='w')
+        current_downloadfolder = tk.Label(grid1, text=f'Auswahl:  {self.download_folder}', font=('Arial', int ( 10 ), 'italic'))
+        current_downloadfolder.grid(column=0, row=2, padx=20, sticky='w')
+        select_download_button = tk.Button(grid1, text='Select...', width=10, command=lambda:self.select_download_folder(current_downloadfolder))
+        select_download_button.grid(column=1, row=2)
+        
+        destinationfolder_lbl = tk.Label(grid1, text='Select your Destination-Folder...', font=('Arial', int ( 10 ), 'bold'))
+        destinationfolder_lbl.grid(column=0, row=3, padx=20, pady=(10,0), sticky='w')
+        current_destinationfolder = tk.Label(grid1, text=f'Auswahl:  {self.destination_folder}', font=('Arial', int ( 10 ), 'italic'))
+        current_destinationfolder.grid(column=0, row=4, padx=20, sticky='w')
+        select_destination_button = tk.Button(grid1, text='Select...', width=10, command=lambda:self.select_destination_folder(current_destinationfolder))
+        select_destination_button.grid(column=1, row=4)
+           
+        
+        
+        # --------- SECOND ROW ---------
+        grid2 = tk.Frame(grid, bd=2)
+        grid2.grid(column=0, row=1, padx=0, sticky='w')
+        
+        extensionsetting_lbl = tk.Label(grid2, text='Extensions', font=('Arial', int ( width / 25 ), 'bold'))
+        extensionsetting_lbl.grid(column=0, row=0, padx=20, pady=(10, 0), sticky='w')
+        
+        extensionsetting_btn = tk.Button(grid2, text='?', width=5, command=self.help)
+        extensionsetting_btn.grid(column=1, row=0, padx=20, pady=(10, 0))
+        
+        sub_grid2 = tk.Frame(grid2, bd=2)
+        sub_grid2.grid(column=0, row=1, padx=0, sticky='w')
+        
+        self.index_categories(sub_grid2)        
+        
+        
+        # --------- THIRD ROW ---------
+        grid3 = tk.Frame(grid, bd=2)
+        grid3.grid(column=0, row=2, padx=0, sticky='w')
+        
+        miscsettings_lbl = tk.Label(grid3, text='Settings', font=('Arial', int ( width / 25 ), 'bold'))
+        miscsettings_lbl.grid(column=0, row=0, padx=20, pady=(10, 0), sticky='w')
+        
+        sub_grid3 = tk.Frame(grid3, bd=2)
+        sub_grid3.grid(column=0, row=1, padx=0, sticky='w')
+        
+        self.init_settings(sub_grid3)
+
+if __name__ == '__main__':
+    runner = File_Handler()
+    root = GUI()
+    root.mainloop()
---
+++ downloadOrganizer.py
@@ -0,0 +1,173 @@
+import os
+import pathlib
+from random import randint
+import sched, time
+import threading
+import shutil
+
+from pandas import interval_range
+
+global downloads_directory 
+global destination_directory 
+global interval 
+
+downloads_directory = f'C:/Users/{os.getlogin}/Downloads'
+destination_directory = f'C:/Users/{os.getlogin}/Downloads'
+interval = 5
+
+# Directories, categories & extensions are saved in 'settings.conf'
+def load_settings():
+    global downloads_directory, destination_directory, interval
+    categories = {} 
+    with open('settings.ini', 'r') as f:
+        lines = f.readlines()
+        line_counter = 0
+        category = ''
+        for line in lines:
+            if line[0] == '_':
+                interval = line.split('_')[1]
+                continue
+            if line[0] == '#':
+                continue
+            if line[0] == '<' and not line == '\n': # downloads folder from conf-file
+                downloads_directory = line.strip().split('<')[1]
+            elif line[0] == '>' and not line == '\n': # destination folder 
+                destination_directory = line.strip().split('>')[1]
+            elif line == '\n': # in case line is empty
+                continue
+            elif line_counter % 2 == 0:
+                category = line.strip()
+            else:    
+                extensions = line.split(',')
+                extensions[len(extensions) - 1] = extensions[len(extensions) - 1].rstrip() # remove \n from last element in array
+                categories[category] = extensions
+                category = ''
+            line_counter += 1
+        categories['Misc'] = None
+    return categories
+
+def change_download_dir(dir):
+    with open('settings.ini', 'r') as f:
+        lines = f.readlines()
+        line_counter = 0
+        index = 0
+        for line in lines:
+            index += 1
+            if line[0] == '_':
+                continue
+            if line[0] == '#':
+                continue
+            if line_counter == 0: 
+                lines[index - 1] = '<' + dir + '\n'
+            elif line == '\n': 
+                continue
+            line_counter += 1
+    with open('settings.ini', 'w') as f:
+        f.writelines(lines)
+
+def change_destination_dir(dir):
+    with open('settings.ini', 'r') as f:
+        lines = f.readlines()
+        line_counter = 0
+        index = 0
+        for line in lines:
+            index += 1
+            if line[0] == '_':
+                continue
+            if line[0] == '#':
+                continue
+            if line_counter == 0: 
+                lines[index] = '>' + dir + '\n'
+            elif line == '\n': 
+                continue
+            line_counter += 1
+    with open('settings.ini', 'w') as f:
+        f.writelines(lines)
+
+def change_interval(interval):
+    with open('settings.ini', 'r') as f:
+        lines = f.readlines()
+        line_counter = 0
+        index = 0
+        for line in lines:
+            index += 1
+            if line[0] == '_':
+                lines[index-1] = '_' + interval + '\n'
+                continue
+            if line[0] == '#':
+                continue
+            if line == '\n': 
+                continue
+            line_counter += 1
+    with open('settings.ini', 'w') as f:
+        f.writelines(lines)
+
+class File_Handler:
+    def __init__(self) -> None:
+        self.categories_extensions = load_settings()
+        self.run = False
+        # self.check_if_created()
+        # self.indexing_files()
+    def check_if_created (self) -> None:
+        for category in self.categories_extensions.keys():
+            if not os.path.exists(f'{destination_directory}/Downloaded {category}'):
+                os.makedirs(f'{destination_directory}/Downloaded {category}')
+    def indexing_files(self) -> None:
+        while self.run:
+            self.down_directory = os.fsencode(downloads_directory)
+            for file in os.listdir(self.down_directory):
+                filename = os.fsdecode(file)
+                f = os.path.join(downloads_directory, filename)
+                if os.path.isfile(f):
+                    self.move_file(filename)
+            time.sleep(int(interval))
+    def move_file(self, filename) -> None:
+        suffix = pathlib.Path(filename).suffix.split('.')[1]
+        temp_category = 'Misc'
+        # fetch category 
+        for k, v in self.categories_extensions.items():
+            if v is not None:
+                for i in v:
+                    if i == suffix:
+                        temp_category = k
+                        break
+        # if still empty:
+        if temp_category == '': 
+            temp_category = 'Misc'
+        # move to desired folder
+        if os.path.exists(f'{destination_directory}/Downloaded {temp_category}/{filename}'):
+            full_file = filename.split('.')
+            fname = full_file[0] + str(randint(10, 100000))
+            full_file = fname + '.' + full_file[1]
+            shutil.move(f'{downloads_directory}/{filename}', f'{destination_directory}/Downloaded {temp_category}/{full_file}')
+        else:
+            shutil.move(f'{downloads_directory}/{filename}', f'{destination_directory}/Downloaded {temp_category}/{filename}')
+    def get_download_directory(self) -> str:
+        return downloads_directory
+    def get_destination_directory(self) -> str:
+        return destination_directory
+    def set_download_directory(self, dir):
+        change_download_dir(dir)
+    def set_destination_directory(self, dir):
+        change_destination_dir(dir)
+    def get_categories_extensions(self):
+        return self.categories_extensions
+    def get_interval(self):
+        return interval
+    def set_interval(self, interval):
+        change_interval(interval)
+    def start(self):
+        self.run = True
+        t = threading.Thread(target=self.indexing_files)
+        t.start()
+    def stop(self):
+        self.run = False
+
+
+
+"""
+<Ziel> 
+> Es soll anfangs in einer GUI ausgewaehlt werden koennen, welche Extensions zu welcher Kategorie gehoeren. <
+> Downloads-Ordner soll ebenfalls ausgewaehlt werden koennen < 
+> Ziel-Order ebenso <
+"""
